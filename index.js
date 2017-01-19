ac74911aea40e547d4cd7a07217cf18a767a64cb
Matt Montgomery <mmontgomery@deseretdigital.com>
2017-01-19 12:46:57 -0700

    Continued work

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const config = require('./config.js');
 
+const { projects = [], apacheConfigDir, buildPath } = config;
 require('dotenv').config();
-
-const buildPath = `${__dirname}/build`;
 
 const Promise = require('bluebird');
 
 const prepRepo = require('./lib/prepRepo');
 const getBranches = require('./lib/getBranches');
 const buildBranches = require('./lib/buildBranches');
+const setupBranches = require('./lib/setupBranches');
+const writeStatus = require('./lib/writeStatus');
+const cleanupBranches = require('./lib/cleanupBranches');
+const now = new Date();
+console.logDate = (...args) => console.log(`[${now.toString()}]`, ...args);
 
-Promise.resolve(config)
+Promise.resolve(projects)
     .map((config) => prepRepo(config, buildPath), { concurrency: 1 })
     .map((...args) => getBranches(...args, buildPath), { concurrency: 1 })
     .map((...args) => buildBranches(...args, buildPath), { concurrency: 1 })
+    .then((branchList) => setupBranches(branchList, config))
+    .then((branchList) => writeStatus(branchList, config))
+    .then((branches) => cleanupBranches(branches, config))
+    .then((...args) => {
+        console.log(...args);
+    })
