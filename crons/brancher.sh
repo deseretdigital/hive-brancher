63356d00ba6eb55c648e940c11ec50acbf08a52f
n8jadams <nateisneato@gmail.com>
2019-07-26 13:38:39 -0600

    Moved brancher to crons, commit it

---
+++ crons/brancher.sh
@@ -0,0 +1,7 @@
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+if ps -ef | grep -v grep | grep hive-brancher/index.js ; then
+    exit 0
+else
+    /usr/local/bin/node /var/www/hive-brancher/index.js
+    exit 0
+fi
