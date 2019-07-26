f093cf76babb967bcd5f2202a3938b3f746bd467
n8jadams <nateisneato@gmail.com>
2019-07-26 13:26:33 -0600

    Added crons with cleanup step

---
+++ crons/cleanup.js
@@ -0,0 +1,53 @@
+
+let {
+  projects,
+  buildPath,
+  apacheConfigDir
+} = require('../config.js');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+let builtFilesToKeep = ['status.json'];
+let apacheConfigsToKeep = [];
+
+// Now, generate those files to keep
+let currentBranches = fs.readFileSync('/var/www/hive-brancher/whitelist.json');
+currentBranches = JSON.parse(currentBranches);
+projects.forEach(p => {
+  builtFilesToKeep.push(`base_${p.repo}`);
+  currentBranches.forEach(cb => {
+    builtFilesToKeep.push(`branch_${p.repo}_${cb.branch}`);
+  });
+});
+currentBranches.forEach(cb => {
+  apacheConfigsToKeep.push(`branch_${cb.branch}.conf`);
+});
+
+// List of files and directories to delete
+let toDelete = [];
+
+fs.readdirSync(buildPath).forEach(builtFile => {
+  if(builtFilesToKeep.indexOf(builtFile) === -1) {
+    toDelete.push(`${buildPath}/${builtFile}`);
+  }
+});
+
+fs.readdirSync(apacheConfigDir).forEach(configFile => {
+  if(apacheConfigsToKeep.indexOf(configFile) === -1) {
+    toDelete.push(`${apacheConfigDir}/${configFile}`);
+  }
+});
+
+// Now, delete them all
+toDelete.forEach(f => {
+  const stat = fs.statSync(f);
+  if(stat.isDirectory()) {
+    console.log(`Deleting files in ${f}...`);
+    execSync(`rm -rf ${f}`);
+  } else if(stat.isFile()) {
+    console.log(`Deleting ${f}...`);
+    fs.unlinkSync(f);
+  }
+});
+
+console.log('Finished!');
---
+++ crons/crontab.example.sh
@@ -0,0 +1,2 @@
+* * * * * /var/www/hive-brancher/crons/brancher.sh > /tmp/hive-brancher.log
+* * * * * /usr/local/bin/node /var/www/hive-brancher/crons/cleanup.js
