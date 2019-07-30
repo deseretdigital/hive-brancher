006a053033c915fb9fbe780aab7e8e460b3632d3
n8jadams <nateisneato@gmail.com>
2019-07-30 10:43:04 -0600

    updated the crontab example

--- crons/crontab.example.sh
+++ crons/crontab.example.sh
@@ -1,2 +1,2 @@
-* * * * * /var/www/hive-brancher/crons/brancher.sh > /tmp/hive-brancher.log
+* * * * * cd /var/www/hive-brancher && ./crons/brancher.sh > /tmp/hive-brancher.log
 * * * * * /usr/local/bin/node /var/www/hive-brancher/crons/cleanup.js
