21b7cb544241c012699c409e6c63592dcae3c183
n8jadams <nateisneato@gmail.com>
2019-08-06 08:45:29 -0600

    Actually prevent the use of a subdomain twice

--- ui/app/Components/App.js
+++ ui/app/Components/App.js
@@ -69,12 +69,13 @@ export default class App extends Component {
       return;
     }
 
-    this.props.whitelist.forEach(wl => {
+    for(let i=0;i<this.props.whitelist.length;i++) {
+      const wl = this.props.whitelist[i];
       if(wl.subdomain === this.state.subdomain) {
         alert(`${wl.subdomain} subdomain is already in use.`);
         return;
       }
-    });
+    }
 
     if(this.state.subdomain === 'master' || this.state.branch === 'master') {
       alert('Master is a reserved branch and subdomain that cannot be used');
