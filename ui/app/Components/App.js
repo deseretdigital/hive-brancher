38260d669437ff8d54e696002c6de7f30b37e4e9
n8jadams <nateisneato@gmail.com>
2019-08-12 16:28:18 -0600

    Should be good stylewise

--- ui/app/Components/App.js
+++ ui/app/Components/App.js
@@ -132,7 +132,7 @@ export default class App extends Component {
                   href={subdomainUrl}
                   target="_blank"
                 >{item.subdomain} ({item.branch}) Created by {item.user} on {item.created}</a>
-                <div>
+                <div className="buttonSeperator">
                   <button className="save" onClick={() => this.copyToClipboard(subdomainUrl)}>Copy Url</button>
                   <button className="delete" onClick={() => this.handleDeleteSubdomain(item)}>Delete</button>
                 </div>
