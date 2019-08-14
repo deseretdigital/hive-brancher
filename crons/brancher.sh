3a56bcdecb4059f55f77294384852331b9f94c28
n8jadams <nateisneato@gmail.com>
2019-08-14 13:57:22 -0600

    Verbose mode

--- crons/brancher.sh
+++ crons/brancher.sh
@@ -2,6 +2,6 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 if ps -ef | grep -v grep | grep hive-brancher/index.js ; then
     exit 0
 else
-    /usr/local/bin/node /var/www/hive-brancher/index.js
+    /usr/local/bin/node /var/www/hive-brancher/index.js -v
     exit 0
 fi
