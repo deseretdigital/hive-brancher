49323315266a4313964ea923cd1986dba14838f2
n8jadams <nateisneato@gmail.com>
2019-08-14 14:38:56 -0600

    Trying again

--- config.jobsExample.js
+++ config.jobsExample.js
@@ -58,7 +58,7 @@ module.exports = {
         },
         {
           proc: 'cp',
-          args: ['/var/dynamic/jobs-branches/branch_ksl-api_master/*', '/var/dynamic/jobs-branches/branch_ksl-api_${branchname}'],
+          args: ['-r', '/var/dynamic/jobs-branches/branch_ksl-api_master/*', '/var/dynamic/jobs-branches/branch_ksl-api_${branchname}'],
           proceedOnFailure: true
         },
         {
