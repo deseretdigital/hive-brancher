a5772544afd38d1579cf66dfce59fc7a8adb693d
n8jadams <nateisneato@gmail.com>
2019-09-27 09:21:22 -0600

    Working cleanup to filter out branches that aren't on remote

--- crons/cleanup.js
+++ crons/cleanup.js
@@ -5,48 +5,65 @@ let {
 } = require('../config.js');
 const fs = require('fs');
 const { execSync } = require('child_process');
+const getFormattedBranches = require('../lib/getFormattedBranches');
 
 let builtFilesToKeep = ['status.json'];
 let apacheConfigsToKeep = [];
 
-// Now, generate those files to keep
-let currentBranches = fs.readFileSync('/var/www/hive-brancher/whitelist.json');
-currentBranches = JSON.parse(currentBranches);
-projects.forEach(p => {
-  builtFilesToKeep.push(`base_${p.repo}`);
-  currentBranches.forEach(cb => {
-    builtFilesToKeep.push(`branch_${p.repo}_${cb.branch}`);
+// Grab all of the current branches from remote (github)
+Promise.resolve(getFormattedBranches())
+  .then(remoteBranches => {
+    let flattenedBranches = [];
+    Object.keys(remoteBranches).forEach(repo => {
+      flattenedBranches.push(remoteBranches[repo]);
+    });
+    return [...new Set(flattenedBranches)];
+  })
+  .then(remoteBranches => {
+    // Remove branches from whitelist.json that aren't on remote
+    let currentBranches = fs.readFileSync('/var/www/hive-brancher/whitelist.json');
+    currentBranches = JSON.parse(currentBranches)
+      .filter(b => b.subdomain === 'master' || remoteBranches.indexOf(b.branch)) !== -1;
+    console.log(currentBranches);
+    return;
+
+    // Now, generate those files to keep
+    projects.forEach(p => {
+      builtFilesToKeep.push(`base_${p.repo}`);
+      currentBranches.forEach(cb => {
+        builtFilesToKeep.push(`branch_${p.repo}_${cb.branch}`);
+      });
+    });
+    currentBranches.forEach(cb => {
+      apacheConfigsToKeep.push(`branch_${cb.branch}.conf`);
+    });
+
+    // List of files and directories to delete
+    let toDelete = [];
+
+    fs.readdirSync(buildPath).forEach(builtFile => {
+      if (builtFilesToKeep.indexOf(builtFile) === -1) {
+        toDelete.push(`${buildPath}/${builtFile}`);
+      }
+    });
+
+    fs.readdirSync(apacheConfigDir).forEach(configFile => {
+      if (apacheConfigsToKeep.indexOf(configFile) === -1) {
+        toDelete.push(`${apacheConfigDir}/${configFile}`);
+      }
+    });
+
+    // Now, delete them all
+    toDelete.forEach(f => {
+      const stat = fs.statSync(f);
+      if (stat.isDirectory()) {
+        console.log(`Deleting files in ${f}...`);
+        execSync(`rm -rf ${f}`);
+      } else if (stat.isFile()) {
+        console.log(`Deleting ${f}...`);
+        fs.unlinkSync(f);
+      }
+    });
+
+    console.log('Finished!');
   });
-});
-currentBranches.forEach(cb => {
-  apacheConfigsToKeep.push(`branch_${cb.branch}.conf`);
-});
-
-// List of files and directories to delete
-let toDelete = [];
-
-fs.readdirSync(buildPath).forEach(builtFile => {
-  if(builtFilesToKeep.indexOf(builtFile) === -1) {
-    toDelete.push(`${buildPath}/${builtFile}`);
-  }
-});
-
-fs.readdirSync(apacheConfigDir).forEach(configFile => {
-  if(apacheConfigsToKeep.indexOf(configFile) === -1) {
-    toDelete.push(`${apacheConfigDir}/${configFile}`);
-  }
-});
-
-// Now, delete them all
-toDelete.forEach(f => {
-  const stat = fs.statSync(f);
-  if(stat.isDirectory()) {
-    console.log(`Deleting files in ${f}...`);
-    execSync(`rm -rf ${f}`);
-  } else if(stat.isFile()) {
-    console.log(`Deleting ${f}...`);
-    fs.unlinkSync(f);
-  }
-});
-
-console.log('Finished!');
--- ui/server.js
+++ ui/server.js
@@ -24,33 +24,6 @@ app.get('/branches', (req, res, next) => {
       res.send(allBranches);
       next();
     });
-    // // Get the branches for each project, but skip ones we indicate
-    // .filter((project) => !project.ignoreBranches)
-    // .map((project) => getBranches(project, null, null, buildPath, false), { concurrency: 1 })
-    // .then((repoList) => {
-    //   const allBranches = {};
-    //   repoList.forEach(repo => {
-    //     repoName = repo.repo;
-    //     repo.branches.forEach(branch => {
-    //       if (
-    //         branch.name !== 'master' &&
-    //         !allBranches[repoName]
-    //       ) {
-    //         allBranches[repoName] = [branch.name];
-    //       } else if (
-    //         branch.name !== 'master' &&
-    //         allBranches[repoName].indexOf(branch.name) === -1
-    //       ) {
-    //         allBranches[repoName].push(branch.name);
-    //       }
-    //     });
-    //   });
-    //   Object.keys(allBranches).forEach((repo) => {
-    //     allBranches[repo].sort();
-    //   });
-    //   res.send(allBranches);
-    //   next();
-    // });
 });
 
 app.listen(port, () => console.log('Listening on port ' + port));
