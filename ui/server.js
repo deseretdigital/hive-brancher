8237b29dbe5c694bd895752763011f5cae7d243e
n8jadams <nateisneato@gmail.com>
2019-09-27 08:54:58 -0600

    Trying out a getFormattedBranches promise

--- ui/server.js
+++ ui/server.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const Promise = require('bluebird');
-const config = require('../config.js');
-const prepRepo = require('../lib/prepRepo');
-const getBranches = require('../lib/getBranches');
-const { projects = [], buildPath } = config;
+const getFormattedBranches = require('../lib/getFormattedBranches');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,34 +19,38 @@ app.post('/save-whitelist', (req, res, next) => {
 });
 
 app.get('/branches', (req, res, next) => {
-  Promise.resolve(projects)
-    // Get the branches for each project, but skip ones we indicate
-    .filter((project) => !project.ignoreBranches)
-    .map((project) => getBranches(project, null, null, buildPath, false), { concurrency: 1 })
-    .then((repoList) => {
-      const allBranches = {};
-      repoList.forEach(repo => {
-        repoName = repo.repo;
-        repo.branches.forEach(branch => {
-          if (
-            branch.name !== 'master' &&
-            !allBranches[repoName]
-          ) {
-            allBranches[repoName] = [branch.name];
-          } else if (
-            branch.name !== 'master' &&
-            allBranches[repoName].indexOf(branch.name) === -1
-          ) {
-            allBranches[repoName].push(branch.name);
-          }
-        });
-      });
-      Object.keys(allBranches).forEach((repo) => {
-        allBranches[repo].sort();
-      });
-      res.send(allBranches);
-      next();
-    });
+  Promise.resolve(resolve => resolve(getFormattedBranches())).then(allBranches => {
+    console.log(allBranches);
+    res.send(allBranches);
+    next();
+  });
+    // // Get the branches for each project, but skip ones we indicate
+    // .filter((project) => !project.ignoreBranches)
+    // .map((project) => getBranches(project, null, null, buildPath, false), { concurrency: 1 })
+    // .then((repoList) => {
+    //   const allBranches = {};
+    //   repoList.forEach(repo => {
+    //     repoName = repo.repo;
+    //     repo.branches.forEach(branch => {
+    //       if (
+    //         branch.name !== 'master' &&
+    //         !allBranches[repoName]
+    //       ) {
+    //         allBranches[repoName] = [branch.name];
+    //       } else if (
+    //         branch.name !== 'master' &&
+    //         allBranches[repoName].indexOf(branch.name) === -1
+    //       ) {
+    //         allBranches[repoName].push(branch.name);
+    //       }
+    //     });
+    //   });
+    //   Object.keys(allBranches).forEach((repo) => {
+    //     allBranches[repo].sort();
+    //   });
+    //   res.send(allBranches);
+    //   next();
+    // });
 });
 
 app.listen(port, () => console.log('Listening on port ' + port));
